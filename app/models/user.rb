fe203eb316ab0e2e66ef85ef352e34776bf0ed7d
fuokuyama16 <fuokuyama16@gmail.com>
2024-08-17 16:45:57 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,7 @@ class User < ApplicationRecord
   validates :last_name,  presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々]+\z/, message: 'は全角（漢字・ひらがな・カタカナ）で入力してください' }
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角（カタカナ）で入力してください' }
   validates :last_name_kana,  presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'は全角（カタカナ）で入力してください' }
-  validates :password, presence: true, length: { minimum: 6 },
-                       format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は英字と数字の両方を含む必要があります' }
-  validates :password_confirmation, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は英字と数字の両方を含む必要があります' }
   validates :birth_date, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
