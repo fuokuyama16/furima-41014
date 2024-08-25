2a6cc3ed95accd2592986ab5879794111d6bf0eb
fuokuyama16 <fuokuyama16@gmail.com>
2024-08-26 08:04:45 +0900

    バリデーション設定

---
+++ app/models/item.rb
@@ -0,0 +1,27 @@
+class Item < ApplicationRecord
+  belongs_to :user
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_cost
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping_day
+  has_one_attached :image
+  validates :name, presence: true
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_cost_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :description, :price, :image, presence: true
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+
+  before_save :calculate_fees_and_profits
+
+  private
+
+  def calculate_fees_and_profits
+    self.fee = (price * 0.1).floor
+    self.profit = (price - fee).floor
+  end
+end
