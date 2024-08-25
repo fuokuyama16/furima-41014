93c16e29d9f1c48b3bcc6fc9421388c269892bb2
fuokuyama16 <fuokuyama16@gmail.com>
2024-08-26 08:07:37 +0900

    商品出品ページ投稿機能作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,36 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+    @categories = Category.all
+    @conditions = Condition.all
+    @shippingcosts = ShippingCost.all
+    @prefectures = Prefecture.all
+    @shippingdays = ShippingDay.all
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.user = current_user
+    @categories = Category.all
+    @conditions = Condition.all
+    @shippingcosts = ShippingCost.all
+    @prefectures = Prefecture.all
+    @shippingdays = ShippingDay.all
+    if @item.save
+      redirect_to root_path
+    else
+      puts @item.errors.full_messages
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_cost_id, :prefecture_id,
+                                 :shipping_day_id, :price, :image, :user_id)
+  end
 end
