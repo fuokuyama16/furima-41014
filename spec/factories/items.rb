d52e15728907be1ee24c71083191c411bae4e1f3
fuokuyama16 <fuokuyama16@gmail.com>
2024-08-26 08:08:20 +0900

    テストコード記述

---
+++ spec/factories/items.rb
@@ -0,0 +1,17 @@
+FactoryBot.define do
+  factory :item do
+    name { 'サンプル商品' }
+    description { '２０１０年頃に買った' }
+    category_id { 2 }
+    condition_id { 2 }
+    shipping_cost_id { 2 }
+    prefecture_id { 2 }
+    shipping_day_id { 2 }
+    price { 300 }
+    association :user, factory: :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+  end
+end
