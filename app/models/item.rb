8955a77d430eee629f553c43489a5dd7a1d0146c
fuokuyama16 <fuokuyama16@gmail.com>
2024-08-26 18:42:22 +0900

    外部キーバリデーション削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,5 +15,4 @@ class Item < ApplicationRecord
   validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :description, :price, :image, presence: true
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-  validates :user, presence: true
 end
