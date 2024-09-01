4df46092659f0c824a75bc9d557c046cb4b1e475
fuokuyama16 <fuokuyama16@gmail.com>
2024-09-01 15:37:00 +0900

    オーダーモデルルーティング削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :orders
  devise_for :users
  root to: "items#index"
  resources :items, only: [:new, :create, :index, :show, :edit, :update, :destroy]
