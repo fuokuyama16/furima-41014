51a63e8100407dc1191ea895cdc72a6dd4b60434
fuokuyama16 <fuokuyama16@gmail.com>
2024-09-01 17:12:31 +0900

    出品していない商品やログアウト時編集不可記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :check_item_owner!, only: [:edit]
   def index
     @items = Item.order(created_at: :desc)
   end
@@ -21,6 +22,24 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def check_item_owner!
+    @item = Item.find(params[:id])
+    redirect_to root_path if @item.user != current_user
   end
 
   private
