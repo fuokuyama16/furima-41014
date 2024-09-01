2bd2fd506d326b170f4bca3a5fb6d19ab950fbce
fuokuyama16 <fuokuyama16@gmail.com>
2024-09-01 15:37:54 +0900

    itemモデル記述削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :user
-  has_one :order
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :condition
