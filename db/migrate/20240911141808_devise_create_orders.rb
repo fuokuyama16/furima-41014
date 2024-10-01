a345d551650804a7319e0ab3b34bc04b357394ac
fuokuyama16 <fuokuyama16@gmail.com>
2024-10-02 00:11:28 +0900

    deviseを使った間違えたテーブルを削除し、orderモデルとテーブル再作成

--- db/migrate/20240911141808_devise_create_orders.rb
+++
@@ -1,3 +0,0 @@
-class DeviseCreateOrders < ActiveRecord::Migration[7.0]
-  # クラスの内容
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_15_144448) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_01_142846) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -40,13 +40,13 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_15_144448) do
   end
 
   create_table "addresses", charset: "utf8", force: :cascade do |t|
+    t.bigint "order_id", null: false
     t.string "postal_code", default: "", null: false
     t.integer "prefecture_id", null: false
     t.string "city", default: "", null: false
     t.string "street", default: "", null: false
     t.string "building"
     t.string "phone_number", default: "", null: false
-    t.bigint "order_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["order_id"], name: "index_addresses_on_order_id"
@@ -68,17 +68,11 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_15_144448) do
   end
 
   create_table "orders", charset: "utf8", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "item_id"
+    t.bigint "user_id", null: false
+    t.bigint "item_id", null: false
+    t.string "token", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "postal_code", default: "", null: false
-    t.integer "prefecture_id", null: false
-    t.string "city", default: "", null: false
-    t.string "street", default: "", null: false
-    t.string "building"
-    t.string "phone_number", default: "", null: false
-    t.string "token", null: false
     t.index ["item_id"], name: "index_orders_on_item_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
